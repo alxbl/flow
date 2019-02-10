d1b2e98182935f507d62b35afe0afcc57af6f29e
Alexandre Beaulieu <alex@segfault.me>
2019-02-10 17:30:09 -0500

    feat(api): Added helmet and development specific configuration.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,12 @@
 const path =  require('path');
+const base = __dirname;
 const fs = require('fs');
 
+const options = require(path.join(base, './config.json'))
+
 const { GraphQLServer } = require('graphql-yoga');
+const helmet = require('helmet');
+
 
 const resolvers = {
   Query: {
@@ -12,16 +17,37 @@ const resolvers = {
 
 };
 
-fs.readFile(path.join(__dirname,  '../schema.graphql'), (e1, schema) => {
-  fs.readFile(path.join(__dirname,  '../model.graphql'), (e2, model) => {
+// Toggle some options if the server is running in development mode.
+if (process.env.NODE_ENV === 'development') {
+  options.cors = true; // Allow CORS to run frontend dev-server.
+  options.playground = '/'; // Allow GraphQL playground for API
+}
+
+function useHelmet(express) {
+  express.use(helmet());
+
+  if (process.env.NODE_ENV != 'development') { // No CSP in development mode to allow GraphQLPlayground.
+
+    express.use(helmet.contentSecurityPolicy({
+      directives: { defaultSrc: ["'self'"], }
+    }))
+  }
+}
+
+// Start the GraphQL server.
+fs.readFile(path.join(base,  '../schema.graphql'), (e1, schema) => {
+  fs.readFile(path.join(base,  '../model.graphql'), (e2, model) => {
     if (e1 || e2) {
       console.error('Error: Failed to process schema.');
      return -1;
     }
-    const typeDefs = schema + model;
+
+    const typeDefs = model + schema;
 
     const srv = new GraphQLServer({ typeDefs, resolvers, });
-    srv.start(() => console.log(`Server running on port 4000`));
+    useHelmet(srv.express);
+
+    srv.start(options, () => console.log(`Accepting connections on port ${options.port}`));
   });
 });
 
